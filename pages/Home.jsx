dc997e8c53d84a1823dbb44f5d62655116bb0bac
bhadmus damilola <90419022+damax-codz@users.noreply.github.com>
2023-05-16 00:26:12 +0100

    update

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -97,7 +97,10 @@ const Home = () => {
                 width={40}
                 height={40}
                 className="cursor-pointer"
-                onClick={() => setTheme(!theme)}
+                onClick={() => {
+                  setTheme(!theme);
+                  closeNav();
+                }}
               />
             </div>
           </div>
